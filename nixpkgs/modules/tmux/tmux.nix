acf356d47f4c09688d117b0c195ae44b5e664f4a
Michael Maurer <mjmaurer777@gmail.com>
2024-05-23 21:09:58 -0400

    Add tmux

---
+++ nixpkgs/modules/tmux/tmux.nix
@@ -0,0 +1,39 @@
+{ pkgs, ... }:
+let
+#   tmux-super-fingers = pkgs.tmuxPlugins.mkTmuxPlugin
+#     {
+#       pluginName = "tmux-super-fingers";
+#       version = "unstable-2023-01-06";
+#       src = pkgs.fetchFromGitHub {
+#         owner = "artemave";
+#         repo = "tmux_super_fingers";
+#         rev = "2c12044984124e74e21a5a87d00f844083e4bdf7";
+#         sha256 = "sha256-cPZCV8xk9QpU49/7H8iGhQYK6JwWjviL29eWabuqruc=";
+#       };
+#     };
+in
+{
+  programs.tmux = {
+    enable = true;
+    # shell = "${pkgs.fish}/bin/fish";
+    terminal = "tmux-256color";
+    historyLimit = 100000;
+    plugins = with pkgs;
+      [
+        # Sensible installed by default
+        # {
+        #   plugin = tmuxPlugins.sensible; 
+        #   extraConfig = ''
+        #     run-shell ${pkgs.tmuxPlugins.sensible.rtp}
+        #   '';
+        # }
+        # {
+        #   plugin = tmux-super-fingers;
+        #   extraConfig = "set -g @super-fingers-key f";
+        # }
+        # tmuxPlugins.better-mouse-mode
+      ];
+    # Nix home-manager tmuxConf includes a bunch of defaults we don't want:
+    extraConfig = builtins.readFile ./tmux.conf;
+  };
+}
