ae8f417841f1c51219fe7a8cec53191fad4f503c
Michael Maurer <mjmaurer777@gmail.com>
2024-06-18 00:16:55 -0400

    Add duplicacy module

--- nixpkgs/machines/earth.nix
+++ nixpkgs/machines/earth.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  imports = [ ../common/common.nix ../common/common-linux.nix ];
+  imports = [ ../common/common.nix ../common/common-linux.nix ../modules/duplicacy/duplicacy.nix ];
 
   programs.bash.sessionVariables = {
     MACHINE_NAME = "earth";
--- nixpkgs/modules/duplicacy/duplicacy.nix
+++ nixpkgs/modules/duplicacy/duplicacy.nix
@@ -1,17 +1,50 @@
 { config, pkgs, ... }:
 let
+  conf = "/etc/duplicacy/duplicacy.conf";
+  backup = "backup -limit-rate 25000 -max-in-memory-entries 1024 -threads 4 -stats";
 in
 {
+  # https://forum.duplicacy.com/t/duplicacy-quick-start-cli/1101
+  # https://forum.duplicacy.com/t/encryption-of-the-storage/1085
   systemd.user.services.duplicacy = {
     Unit.Description = "Duplicacy backup service";
-    Install.WantedBy = [ "default.target" ];
     Service = {
-      Restart = "on-failure";
-      ExecStart = "${pkgs.writeShellScript "run-duplicacy" ''
+      Restart = "no";
+      WorkingDirectory = config.home.homeDirectory;
+      ExecStart = pkgs.writeShellScript "run-duplicacy" ''
         #!/bin/bash
-        #ENV=var
-        ${pkgs.duplicacy}/bin/duplicacy
-      ''}";
+        ${pkgs.duplicacy}/bin/duplicacy ${backup}
+      '';
+      EnvironmentFile = conf;
     };
   };
-};
+
+  systemd.user.timers.duplicacy= {
+    Unit.Description = "Timer for Duplicacy backup service";
+    Timer = {
+      Unit = "duplicacy.service";
+      OnCalendar = "Mon *-*-* 05:00:00 America/New_York";
+      Persistent = true; # Catch up on missed runs
+    };
+    Install.WantedBy = [ "timers.target" ];
+  };
+
+  programs.bash = {
+    # Must call duplicacy-activate to start the timer (or reboot)
+    shellAliases = {
+      duplicacy-activate = "systemctl --user start duplicacy.timer";
+      duplicacy-log = "journalctl --user -fu duplicacy.service";
+      duplicacy-backup = "systemctl --user start duplicacy.service";
+      duplicacy-status = "systemctl --user list-timers";
+      duplicacy-backup-init = ''
+        cd && ${pkgs.duplicacy}/bin/duplicacy backup -limit-rate 20000 -max-in-memory-entries 1024 -threads 4 -stats -t initial
+      '';
+      # nas is the 'snapshot id' (repository id?) for the backup
+      # the b2 bucket will be the 'storage', which can contain multiple repos
+      duplicacy-init = ''
+        source ${conf}
+        cd && ${pkgs.duplicacy}/bin/duplicacy init -encrypt -zstd nas b2://$BUCKET_NAME
+      '';
+    };
+  };
+}
