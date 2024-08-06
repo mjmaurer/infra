a3723a88cb71889ede0c35aac814323a8bcf5741
Michael Maurer <mjmaurer777@gmail.com>
2024-08-06 17:11:27 -0400

    Add aerospace and fix tmux:wq

--- nixpkgs/common/common-mac.nix
+++ nixpkgs/common/common-mac.nix
@@ -3,6 +3,8 @@
 {
   imports = [
     ../modules/aider/aider.nix
+    ../modules/aerospace/aerospace.nix
+    ../modules/tmux/tmux.nix
   ];
 
   home.homeDirectory = "/Users/${config.home.username}";
