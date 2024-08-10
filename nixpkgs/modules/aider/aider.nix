cd13ce5ad4381870d32084a47b583a4617b9a53a
Michael Maurer <mjmaurer777@gmail.com>
2024-08-09 23:28:49 -0400

    aider aliases

--- nixpkgs/modules/aider/aider.nix
+++ nixpkgs/modules/aider/aider.nix
@@ -10,4 +10,14 @@ in
       source = ./aider.conf.yml;
     };
   };
+
+  programs.bash = {
+    shellAliases = {
+      aid = "aider --4";
+      aider = "aider --4";
+      aids = "aider --sonnet";
+      aidf = "aider --4";
+      aidfo = "aider --4o";
+    };
+  };
 }
