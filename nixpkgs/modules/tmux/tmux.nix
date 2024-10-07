adfe314e84f7abea9333bac77446fce44ffa4421
Michael Maurer <mjmaurer777@gmail.com>
2024-10-06 20:47:07 -0400

    Change Tmux shell to zsh

--- nixpkgs/modules/tmux/tmux.nix
+++ nixpkgs/modules/tmux/tmux.nix
@@ -1,22 +1,22 @@
 { pkgs, ... }:
 let
-#   tmux-super-fingers = pkgs.tmuxPlugins.mkTmuxPlugin
-#     {
-#       pluginName = "tmux-super-fingers";
-#       version = "unstable-2023-01-06";
-#       src = pkgs.fetchFromGitHub {
-#         owner = "artemave";
-#         repo = "tmux_super_fingers";
-#         rev = "2c12044984124e74e21a5a87d00f844083e4bdf7";
-#         sha256 = "sha256-cPZCV8xk9QpU49/7H8iGhQYK6JwWjviL29eWabuqruc=";
-#       };
-#     };
+  #   tmux-super-fingers = pkgs.tmuxPlugins.mkTmuxPlugin
+  #     {
+  #       pluginName = "tmux-super-fingers";
+  #       version = "unstable-2023-01-06";
+  #       src = pkgs.fetchFromGitHub {
+  #         owner = "artemave";
+  #         repo = "tmux_super_fingers";
+  #         rev = "2c12044984124e74e21a5a87d00f844083e4bdf7";
+  #         sha256 = "sha256-cPZCV8xk9QpU49/7H8iGhQYK6JwWjviL29eWabuqruc=";
+  #       };
+  #     };
 in
 {
   programs.tmux = {
     enable = true;
-    # shell = "${pkgs.fish}/bin/fish";
-    terminal = "screen-256color";
+    shell = "${pkgs.zsh}/bin/zsh";
+    # terminal = "screen-256color";
     historyLimit = 100000;
     plugins = with pkgs;
       [
