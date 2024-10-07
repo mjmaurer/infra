7112c6483ab50fb18c58c843827a2d6c0db84397
Michael Maurer <mjmaurer777@gmail.com>
2024-10-06 23:33:05 -0400

    Move aider and aerospace to modules

--- nixpkgs/machines/bobby.nix
+++ nixpkgs/machines/bobby.nix
@@ -3,9 +3,11 @@
 {
   imports = [ ../common/linux.nix ];
 
-  programs.bash.sessionVariables = {
-    MACHINE_NAME = "bobby";
-    DEPLOY_SHARED_MOUNT = "${config.home.homeDirectory}/deploy/shared-data";
-    DEPLOY_MODELS_MOUNT = "${config.home.homeDirectory}/deploy/models";
+  modules.commonShell = {
+    machineName = "bobby";
+    sessionVariables = {
+      DEPLOY_SHARED_MOUNT = "${config.home.homeDirectory}/deploy/shared-data";
+      DEPLOY_MODELS_MOUNT = "${config.home.homeDirectory}/deploy/models";
+    };
   };
 }
