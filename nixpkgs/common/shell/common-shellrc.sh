73d5b1a85f15c19ffa9c4478a2df3380b2bb99f4
Michael Maurer <mjmaurer777@gmail.com>
2024-11-22 20:41:47 -0500

    feat(shell): enhance file operation commands and add git exclude utility

--- nixpkgs/common/shell/common-shellrc.sh
+++ nixpkgs/common/shell/common-shellrc.sh
@@ -40,26 +40,81 @@ export FZF_DEFAULT_OPTS="
   --bind='pgup:preview-half-page-up'
 "
 
+git_local_exclude() {
+  # Add's a file to .git/info/exclude if .git exists
+  if [ -z ".git" ]; then
+    echo "Git repo not found"
+    return
+  fi
+  local to_exclude=$(fd --strip-cwd-prefix . | fzf --header 'Select file to exclude from git')
+  if [ -z "$to_exclude" ]; then
+    echo "No file to exclude selected"
+    return
+  fi
+  echo "$to_exclude" >> .git/info/exclude
+}
+
 cpf() {
   # Copies a file or directory given a name. Uses FZF to select the file or directory.
-  if [ -z "$1" ]; then
-    echo "No file or dir name provided"
-    return
-  fi
-  local parent=$(fd --type d --strip-cwd-prefix . | fzf --header 'Select parent directory')
-  if [ -z "$parent" ]; then
-    echo "No parent directory selected"
-    return
-  fi
+
   local to_copy=$(fd --strip-cwd-prefix . | fzf --header 'Select file or dir to copy')
   if [ -z "$to_copy" ]; then
     echo "No file to copy selected"
     return
   fi
-  cp -r "$to_copy" "$parent/$1"
+
+  local parent=$(fd --type d --strip-cwd-prefix . | fzf --header 'Select parent directory')
+  if [ -z "$parent" ]; then
+    echo "No parent directory selected"
+    return
+  fi
+
+  # Prompt for name via stdin:
+  local name
+  if [ -n "$ZSH_VERSION" ]; then
+    vared -p "Name: " name
+  else
+    read -p "Name: " -e -i "$name" name
+  fi
+  if [ -z "$name" ]; then
+    echo "No file or dir name provided"
+    return
+  fi
+
+  # Get extensions (or lack thereof) for both files
+  local to_copy_ext="${to_copy##*.}"
+  local name_ext="${name##*.}"
+  if [[ "$to_copy" != *"."* ]]; then
+    to_copy_ext=""
+  fi
+  if [[ "$name" != *"."* ]]; then
+    name_ext=""
+  fi
+  if [[ "$to_copy_ext" != "$name_ext" ]]; then
+    echo "Error: File extensions must match"
+    return 1
+  fi
+
+  cp -r "$to_copy" "$parent/$name"
 }
 
-mk() {
+mkf() {
+  local name=$1
+  if [ -z "$name" ]; then
+    if [ -n "$ZSH_VERSION" ]; then
+      vared -p "Name: " name
+    else
+      read -p "Name: " -e -i "$name" name
+    fi
+    if [ -z "$name" ]; then
+      echo "No file or dir name provided"
+      return
+    fi
+  fi
+  # Add trailing slash if no extension and doesn't already end in slash
+  if [[ "$name" != *"."* ]] && [[ "$name" != */ ]]; then
+    name="${name}/"
+  fi
   # Creates a file or directory given a name. Uses FZF to select the parent directory.
   # It determines whether to create a file or directory based on whether the name ends with a slash.
   local dir=$(fd --type d --strip-cwd-prefix . | fzf --header 'Select parent directory')
@@ -93,7 +148,7 @@ mvf() {
   mv "$file" "$new_dir"
 }
 
-rename() {
+rnf() {
   local file=$(fd --strip-cwd-prefix . | fzf --header 'Select file or dir to rename')
   if [ -z "$file" ]; then
     echo "No file or dir selected"
