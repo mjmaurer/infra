966cd8163adc9dee36a1c25a58a2ac29f1a82665
Michael Maurer <mjmaurer777@gmail.com>
2024-11-26 19:58:30 -0500

    refactor(py): restructure python flake with lib exports

--- flakes/python/flake.nix
+++ flakes/python/flake.nix
@@ -3,30 +3,22 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }: {
-    readme = ''
-      Debugging:
-      ```zsh
-      bugpyw / bugpy
-      ```
-    '';
-  } //
-  flake-utils.lib.eachDefaultSystem
-    (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-        python = pkgs.python312;
-      in
-      {
-        packages.lang = python;
-        packages.default = with pkgs; [
-          python
-          python312Packages.debugpy
-          # (python.withPackages (python-pkgs: [
-          #   python-pkgs.debugpy
-          # ]))
+  outputs = { self, nixpkgs, flake-utils }:
+    {
+      lib = {
+        readme = ''
+          Debugging:
+          ```zsh
+          bugpyw / bugpy
+          ```
+        '';
+
+        lang = pkgs: pkgs.python312;
+        python = self.lang;
+        packages = pkgs: with pkgs; [
+          (self.python pkgs)
+          (self.python pkgs).pkgs.debugpy
+
           (writeShellScriptBin "bugpyw" ''
             echo "python -Xfrozen_modules=off -m debugpy --listen 5678 --wait-for-client $@"
             echo "May need to activate venv\n"
@@ -39,6 +31,6 @@
             exec python -Xfrozen_modules=off -m debugpy --listen 5678 "$@"
           '')
         ];
-      }
-    );
+      };
+    };
 }
