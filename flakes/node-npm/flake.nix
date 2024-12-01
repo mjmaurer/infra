f5193615f8752af7e942a9c202493532cec6bb19
Michael Maurer <mjmaurer777@gmail.com>
2024-12-01 18:34:25 -0500

    feat: add node/npm development flake with yarn override

---
+++ flakes/node-npm/flake.nix
@@ -0,0 +1,24 @@
+{
+  outputs = { self }:
+    {
+      lib = {
+        readme = ''
+        '';
+
+        overlays = [
+          (final: prev: {
+            yarn = prev.yarn.override {
+              nodejs = self.lib.mkNode final;
+            };
+          })
+        ];
+
+        mkNode = pkgs: pkgs.nodejs_23;
+        mkLang = self.lib.mkNode;
+        mkPackages = pkgs: with pkgs; [
+          (self.lib.mkNode pkgs)
+          yarn
+        ];
+      };
+    };
+}
