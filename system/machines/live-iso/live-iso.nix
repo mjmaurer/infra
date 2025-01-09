ff7f4dd44dcac5521181a5c268d63953099ee576
Michael Maurer <mjmaurer777@gmail.com>
2025-01-08 22:55:33 -0500

    refactor: reorganize live ISO files into system/machines directory

--- system/machines/live-iso/live-iso.nix
+++ system/machines/live-iso/live-iso.nix
@@ -1,16 +1,21 @@
 { nixpkgs ? <nixpkgs>, system ? "x86_64-linux" }:
-let configuration =
+# TODO: https://github.com/dmadisetti/.dots/blob/template/nix/machines/momento.nix
+let
+  configuration =
     { pkgs, ... }:
-    { imports = [
+    {
+      imports = [
         "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+        # Provide an initial copy of the NixOS channel so that the user
+        # doesn't need to run "nix-channel --update" first.
         "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
-        ../system/programs.nix
+        ../../modules/_base.nix
       ];
       environment.systemPackages = with pkgs; [
         (writeScriptBin "partition" (builtins.readFile ./partition))
         git
       ];
     };
-    iso-image = import "${nixpkgs}/nixos" { inherit system configuration; };
+  iso-image = import "${nixpkgs}/nixos" { inherit system configuration; };
 in
 iso-image.config.system.build.isoImage
