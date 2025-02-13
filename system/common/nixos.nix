154f50d9c917b1721ee4f1b71894f17e7ac96ecb
Michael Maurer <mjmaurer777@gmail.com>
2025-02-13 14:07:02 -0500

    feat(system): reorganize module imports and remove default.nix

--- system/common/nixos.nix
+++ system/common/nixos.nix
@@ -2,6 +2,18 @@
   # Never change this here.
   system.stateVersion = lib.mkDefault "24.11";
 
-  imports = [ ./_base.nix ../modules/programs.nix ];
+  imports = [
+    ./_base.nix
+
+    ../modules/sway.nix
+    ../modules/crypt.nix
+    ../modules/fonts.nix
+    ../modules/audio.nix
+    # Move to users?:
+    ../modules/sudo.nix
+    # Look for others in alice's flake
+
+    ../modules/boot.nix
+  ];
 }
 
