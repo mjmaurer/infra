a12ca0dda567d47e1a6cdf4368f2c059aefbba62
Michael Maurer <mjmaurer777@gmail.com>
2025-02-28 00:26:14 -0500

    feat(aider): add custom model configs and improve configuration

--- home-manager/modules/aider/aider.nix
+++ home-manager/modules/aider/aider.nix
@@ -1,5 +1,7 @@
 { lib, config, pkgs, ... }:
-let cfg = config.modules.aider;
+let
+  cfg = config.modules.aider;
+  aider-package = import ./aider-deriv.nix { inherit pkgs lib; };
 in {
   options.modules.aider = { enable = lib.mkEnableOption "aider"; };
 
@@ -7,10 +9,12 @@ in {
 
   config = lib.mkIf cfg.enable {
     # Include playwright for web requests
-    home.packages = [ pkgs.aider-chat.withPlaywright ];
+    home.packages = [ aider-package.withPlaywright ];
     home.file = {
       ".config/aider/.aiderignore" = { source = ./.aiderignore; };
       ".aider.conf.yml" = { source = ./aider.conf.yml; };
+      ".aider.model.settings.yml" = { source = ./aider-models.conf.yml; };
+      # ".aider.model.metadata.json" = { source = ./aider-registry.json; };
     };
 
     modules.commonShell = {
