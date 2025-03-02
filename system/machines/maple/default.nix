82a53fac868b5a22450e0743522cc6c889eb271c
Michael Maurer <mjmaurer777@gmail.com>
2025-03-02 11:48:31 -0500

    fix: move intel hybrid codec config to top-level config

--- system/machines/maple/default.nix
+++ system/machines/maple/default.nix
@@ -68,12 +68,13 @@
           # vaapiVdpau # think VDPAU is just for nvidia
           # libvdpau-va-gl # compat layer that translates VDPAU calls to VA-API (va-api is natively supported by quicksync)
         ];
-        nixpkgs.config.packageOverrides = pkgs: {
-          # Hybrid driver might provide VP9 for skylake, but this was also archived a long time ago.
-          # Probably don't need VP9 (it's video codec), and could comment out if trouble
-          intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
-        };
       };
+    };
+
+    nixpkgs.config.packageOverrides = pkgs: {
+      # Hybrid driver might provide VP9 for skylake, but this was also archived a long time ago.
+      # Probably don't need VP9 (it's video codec), and could comment out if trouble
+      intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
     };
 
     # This assumes boot parition is unencrypted, and root parititon is encrypted with luks.
