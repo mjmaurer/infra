114bd8865574377780964c90d3d5bf54b8839032
Michael Maurer <mjmaurer777@gmail.com>
2025-03-02 22:40:08 -0500

    fix(audio): disable pipewire and ensure pulseaudio is used

--- system/modules/audio.nix
+++ system/modules/audio.nix
@@ -1,5 +1,6 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
+  services.pipewire.enable = lib.mkForce false; # Seems to be enabled by default
   hardware.pulseaudio.enable = true;
   hardware.pulseaudio.package = pkgs.pulseaudioFull;
 }
