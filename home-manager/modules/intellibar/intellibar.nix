e2853adc814812e0fe43bae1bc41c6c96299be9c
Michael Maurer <mjmaurer777@gmail.com>
2025-05-03 20:14:41 -0400

    feat(intellibar): Add configuration with Openrouter models

---
+++ home-manager/modules/intellibar/intellibar.nix
@@ -0,0 +1,46 @@
+{
+  osConfig ? null,
+  lib,
+  config,
+  pkgs-latest,
+  ...
+}:
+let
+  cfg = config.modules.intellibar;
+in
+# NOTE: An overlay is defined in `lib/system.nix` to add google deps
+# aider-package = import ./aider-deriv.nix {
+#   inherit lib;
+#   pkgs = pkgs-latest;
+# };
+{
+  options.modules.intellibar = {
+    enable = lib.mkEnableOption "intellibar";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.file = {
+      ".intellibar/instructions/" = {
+        source = ./instructions;
+        recursive = true;
+      };
+      ".intellibar/models/flash-thinking-25-preview.yaml" = {
+        source =
+          config.lib.file.mkOutOfStoreSymlink
+            osConfig.sops.templates."flash-thinking-25-preview.yaml".path;
+      };
+      ".intellibar/models/flash-25-preview.yaml" = {
+        source = config.lib.file.mkOutOfStoreSymlink osConfig.sops.templates."flash-25-preview.yaml".path;
+      };
+      ".intellibar/models/pro-25-preview.yaml" = {
+        source = config.lib.file.mkOutOfStoreSymlink osConfig.sops.templates."pro-25-preview.yaml".path;
+      };
+      ".intellibar/models/o3.yaml" = {
+        source = config.lib.file.mkOutOfStoreSymlink osConfig.sops.templates."o3.yaml".path;
+      };
+      ".intellibar/models/o4-mini.yaml" = {
+        source = config.lib.file.mkOutOfStoreSymlink osConfig.sops.templates."o4-mini.yaml".path;
+      };
+    };
+  };
+}
--- system/modules/sops/default.nix
+++ system/modules/sops/default.nix
@@ -72,8 +72,56 @@ in
             apiKeyDeepseek = {
               sopsFile = pcSopsFile;
             };
+            apiKeyOpenrouter = {
+              sopsFile = pcSopsFile;
+            };
           };
         templates = {
+          "flash-thinking-25-preview.yaml" = {
+            owner = config.users.users.${username}.name;
+            content = ''
+              name: Gemini Flash Thinking 2.5
+              id: google/gemini-2.5-flash-preview:thinking
+              apiKey: ${config.sops.placeholder.apiKeyOpenrouter}
+              url: https://openrouter.ai/api/v1
+            '';
+          };
+          "flash-25-preview.yaml" = {
+            owner = config.users.users.${username}.name;
+            content = ''
+              name: Gemini Flash 2.5
+              id: google/gemini-2.5-flash-preview
+              apiKey: ${config.sops.placeholder.apiKeyOpenrouter}
+              url: https://openrouter.ai/api/v1
+            '';
+          };
+          "pro-25-preview.yaml" = {
+            owner = config.users.users.${username}.name;
+            content = ''
+              name: Gemini Pro 2.5
+              id: google/gemini-2.5-pro-preview-03-25
+              apiKey: ${config.sops.placeholder.apiKeyOpenrouter}
+              url: https://openrouter.ai/api/v1
+            '';
+          };
+          "o3.yaml" = {
+            owner = config.users.users.${username}.name;
+            content = ''
+              name: o3 
+              id: openai/o3 
+              apiKey: ${config.sops.placeholder.apiKeyOpenrouter}
+              url: https://openrouter.ai/api/v1
+            '';
+          };
+          "o4-mini.yaml" = {
+            owner = config.users.users.${username}.name;
+            content = ''
+              name: o4-mini
+              id: openai/o4-mini
+              apiKey: ${config.sops.placeholder.apiKeyOpenrouter}
+              url: https://openrouter.ai/api/v1
+            '';
+          };
           "shell.env" = {
             # mode = "0400";
             # group = config.users.groups.${username}.name;
