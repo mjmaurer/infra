b797448d664f272c1d224ec32069b4700e02d4bf
Michael Maurer <mjmaurer777@gmail.com>
2025-05-04 21:25:51 -0400

    feat(home-manager): Add activation step to clone infra repo

--- home-manager/common/nixos.nix
+++ home-manager/common/nixos.nix
@@ -1,14 +1,17 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports = [ ./linux.nix ];
 
   modules.commonShell = { };
 
-  # services = {
-  #   gpg-agent = {
-  #     enable = true;
-  #     pinentryFlavor = "gtk2";
-  #   };
-  # };
+  home.activation.cloneInfra = lib.hm.dag.after [ "writeBoundary" ] {
+    text = ''
+      if [ ! -d "$HOME/infra/.git" ]; then
+        echo "Cloning infra repo into $HOME/infra"
+        git clone https://github.com/mjmaurer/infra "$HOME/infra"
+      fi
+    '';
+    deps = [ pkgs.git ];
+  };
 }
