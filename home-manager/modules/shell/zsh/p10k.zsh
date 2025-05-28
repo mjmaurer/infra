0c5c276ef97dd3d89adcc5775e0a6f5714e0f597
Michael Maurer <mjmaurer777@gmail.com>
2025-05-27 22:43:04 -0400

    feat(shell/zsh): Move command execution time to right prompt

--- home-manager/modules/shell/zsh/p10k.zsh
+++ home-manager/modules/shell/zsh/p10k.zsh
@@ -107,6 +107,7 @@
   # last prompt line gets hidden if it would overlap with left prompt.
   typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
     # status                  # exit code of the last command
+    command_execution_time  # duration of the last command
     background_jobs         # presence of background jobs
     #asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
     virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
@@ -163,7 +164,6 @@
     per_directory_history   # Oh My Zsh per-directory-history local/global indicator
     # cpu_arch              # CPU architecture
     time                    # current time
-    command_execution_time  # duration of the last command
     # ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
