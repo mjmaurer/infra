dc4a686dd35a39de8e1f657964d3425e58688440
Michael Maurer <mjmaurer777@gmail.com>
2025-05-30 23:47:25 -0400

    chore(aichat): Update default model and refactor aliases

--- home-manager/modules/aichat/aichat.nix
+++ home-manager/modules/aichat/aichat.nix
@@ -43,8 +43,7 @@ in
 
     modules.commonShell = {
       shellAliases = {
-        "a" = "aichat --model gemini:gemini-2.5-flash-preview-04-17 -e";
-        "ai" = "aichat --role quick";
+        "aicq" = "aichat --role quick";
         "gcai" = "git-commit-ai.sh";
       };
     };
