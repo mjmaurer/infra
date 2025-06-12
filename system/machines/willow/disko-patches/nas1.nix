4e171c8fb7e2b6dbb39b80a40ad8c5b8cfc6c847
Michael Maurer <mjmaurer777@gmail.com>
2025-06-12 19:42:36 -0400

    feat(disko): Add second NAS disk and ZFS improvements

--- system/machines/willow/disko-patches/nas1.nix
+++ system/machines/willow/disko-patches/nas1.nix
@@ -1,39 +1,48 @@
+
+# See: https://github.com/nix-community/disko/blob/master/example/zfs-with-vdevs.nix
+# To setup vdevs
 let
+  # [0, 0] 
   nasDisk1 = "/dev/disk/by-id/wwn-0x50014ee2c13633f8";
+  # [0, 2] 
+  nasDisk2 = "/dev/disk/by-id/wwn-0x50014ee26a4a1377";
   nasPoolName = "znas";
-in
-{
-  disko.devices = {
-    disk = {
-      nas1 = {
-        device = nasDisk1;
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            luks = {
-              name = "nas1-luks";
-              label = "nas1-luks";
-              size = "100%";
-              content = {
-                type = "luks";
-                name = "nas1-luks";
-                extraFormatArgs = [
-                  "--pbkdf argon2i"
-                  "--use-random"
-                ];
-                initrdUnlock = true;
-                extraOpenArgs = [ "--allow-discards" ];
-                passwordFile = "/tmp/disk.key";
-                content = {
-                  type = "zfs";
-                  pool = nasPoolName;
-                };
-              };
+
+  mkNasDisk = devicePath: nameSuffix: {
+    device = devicePath;
+    type = "disk";
+    content = {
+      type = "gpt";
+      partitions = {
+        luks = {
+          name = "${nameSuffix}-luks";
+          label = "${nameSuffix}-luks";
+          size = "100%";
+          content = {
+            type = "luks";
+            name = "${nameSuffix}-luks";
+            extraFormatArgs = [
+              "--pbkdf argon2i"
+              "--use-random"
+            ];
+            initrdUnlock = true;
+            extraOpenArgs = [ "--allow-discards" ];
+            passwordFile = "/tmp/disk.key";
+            content = {
+              type = "zfs";
+              pool = nasPoolName;
             };
           };
         };
       };
+    };
+  };
+in
+{
+  disko.devices = {
+    disk = {
+      nas1 = mkNasDisk nasDisk1 "nas1";
+      nas2 = mkNasDisk nasDisk2 "nas2";
     };
     zpool = {
       "${nasPoolName}" = {
@@ -47,11 +56,16 @@ in
         };
         options = {
           ashift = "12";
+          autoexpand = "on";
         };
         datasets = {
           "nas" = {
             type = "zfs_fs";
             mountpoint = "/${nasPoolName}";
+            options = {
+              xattr = "sa";
+              atime = "off";
+            };
           };
         };
       };
