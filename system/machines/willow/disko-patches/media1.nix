a42e1e223de509ba8b2fd8baf4129142cec53135
Michael Maurer <mjmaurer777@gmail.com>
2025-06-29 03:07:53 -0400

    feat(willow): Add second media disk to disko config

--- system/machines/willow/disko-patches/media1.nix
+++ system/machines/willow/disko-patches/media1.nix
@@ -1,6 +1,14 @@
 let
+  # [1,3]
   mediaDisk1 = "/dev/disk/by-id/wwn-0x5000c500869bbf27";
-  mediaMnt = "/mnt/media/disk1";
+  # TODO: So because disko sets rootMountPoint to /mnt during `disko run`,
+  # this is mounted to /mnt/mnt/media1 instead of /mnt/media1 (I think to resolve issues during installation)
+  # This is rectified during boot, but the directories aren't cleaned up.
+  # Could override
+  mediaMnt1 = "/mnt/media/disk1";
+  # [1,4]
+  mediaDisk2 = "/dev/disk/by-id/wwn-0x5000c50085565ddb";
+  mediaMnt2 = "/mnt/media/disk2";
 in
 {
   disko.devices = {
@@ -18,9 +26,34 @@ in
               content = {
                 type = "filesystem";
                 format = "xfs";
-                mountpoint = mediaMnt;
+                mountpoint = mediaMnt1;
                 mountOptions = [
                   "defaults"
+                  "nofail"
+                  # "pquota"
+                ];
+              };
+            };
+          };
+        };
+      };
+      media2 = {
+        device = mediaDisk2;
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            disk = {
+              name = "media2";
+              label = "media2";
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "xfs";
+                mountpoint = mediaMnt2;
+                mountOptions = [
+                  "defaults"
+                  "nofail"
                   # "pquota"
                 ];
               };
