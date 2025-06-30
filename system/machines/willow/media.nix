214b296992d5906f5f98b24719ce6e98a6710ed2
Michael Maurer <mjmaurer777@gmail.com>
2025-06-29 23:37:54 -0400

    feat(willow): Add SnapRAID configuration

--- system/machines/willow/media.nix
+++ system/machines/willow/media.nix
@@ -13,6 +13,35 @@
     usen = { };
   };
 
+  services.snapraid = {
+    enable = true;
+    sync.interval = "Tue,Fri *-*-* 03:00:00 America/New_York";
+    scrub.interval = "Wed *-*-* 03:00:00 America/New_York";
+    parityFiles = [
+      "/mnt/media/parity1/snapraid.parity"
+    ];
+    # Content files are used to track which files are in the snapraid pool
+    # Choose three for backups purposes
+    contentFiles = [
+      "/var/snapraid.content"
+      "/mnt/media/disk1/d1-snapraid.content"
+      "/mnt/media/disk2/d2-snapraid.content"
+    ];
+    dataDisks = {
+      d1 = "/mnt/media/disk1";
+      d2 = "/mnt/media/disk2";
+    };
+    exclude = [
+      "*.unrecoverable"
+      "/tmp/"
+      "/lost+found/"
+      "downloads/"
+      "appdata/"
+      "*.!sync"
+      "/.snapshots/"
+    ];
+  };
+
   modules.mergerfs = {
     enable = true;
     fsName = "media";
