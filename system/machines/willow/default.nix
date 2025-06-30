c5b1174031fa39ac58f350846f20cb5d168dfe4c
Michael Maurer <mjmaurer777@gmail.com>
2025-06-30 00:35:52 -0400

    refactor(willow): Rename media filesystem config

--- system/machines/willow/default.nix
+++ system/machines/willow/default.nix
@@ -14,7 +14,7 @@
     ../../modules/samba/smb-server.nix
 
     ./hardware-configuration.nix
-    ./media/media-filesystem.nix
+    ./media/filesystem.nix
 
     ./disko.nix
     ./disko-patches/media1.nix
