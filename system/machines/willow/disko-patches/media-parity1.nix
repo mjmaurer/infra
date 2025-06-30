4d186ff58ee8f6dd53f819ee8fbf3cf170f58c01
Michael Maurer <mjmaurer777@gmail.com>
2025-06-29 23:26:24 -0400

    feat(willow): Add disko configuration for media parity disk 1

---
+++ system/machines/willow/disko-patches/media-parity1.nix
@@ -0,0 +1,35 @@
+let
+  # [1,1]
+  mediaParityDisk1 = "/dev/disk/by-id/wwn-0x5000cca260ed767d";
+  mediaParityMnt1 = "/mnt/media/parity1";
+in
+{
+  disko.devices = {
+    disk = {
+      media1 = {
+        device = mediaParityDisk1;
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            disk = {
+              name = "mediaparity1";
+              label = "mediaparity1";
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "xfs";
+                mountpoint = mediaParityMnt1;
+                mountOptions = [
+                  "defaults"
+                  "nofail"
+                  # "pquota"
+                ];
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
