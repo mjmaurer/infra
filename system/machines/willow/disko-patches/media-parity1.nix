8d99486248a1796d20bd372ce8b67910419cb865
Michael Maurer <mjmaurer777@gmail.com>
2025-07-06 16:38:51 -0400

    feat(willow): Use variable for media parity disk name

--- system/machines/willow/disko-patches/media-parity1.nix
+++ system/machines/willow/disko-patches/media-parity1.nix
@@ -2,19 +2,20 @@ let
   # [1,0]
   mediaParityDisk1 = "/dev/disk/by-id/wwn-0x5000cca260ed767d";
   mediaParityMnt1 = "/mnt/media/parity1";
+  mediaParityName1 = "mediaparity1";
 in
 {
   disko.devices = {
     disk = {
-      media1 = {
+      ${mediaParityName1} = {
         device = mediaParityDisk1;
         type = "disk";
         content = {
           type = "gpt";
           partitions = {
             disk = {
-              name = "mediaparity1";
-              label = "mediaparity1";
+              name = mediaParityName1;
+              label = mediaParityName1;
               size = "100%";
               content = {
                 type = "filesystem";
