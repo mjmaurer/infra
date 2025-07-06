27025be038ad60dd1378a38b49dc52aa098a307a
Michael Maurer <mjmaurer777@gmail.com>
2025-07-06 13:19:21 -0400

    feat: Add new virtualisation module

---
+++ system/modules/virtualisation.nix
@@ -0,0 +1,46 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.modules.virt;
+in
+{
+  options.modules.virt = {
+    withPodman = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable Podman support";
+    };
+    withDocker = lib.mkEnableOption "Docker support";
+
+    enableNvidia = lib.mkEnableOption "Enable NVIDIA GPU support for containers";
+  };
+
+  config = {
+    systemPackages = lib.mkIf cfg.withPodman [
+      pkgs.podman-tui
+    ];
+
+    virtualisation = {
+      oci-containers.backend = if cfg.withPodman then "podman" else "docker";
+
+      podman = lib.mkIf cfg.withPodman {
+        enable = true;
+        enableNvidia = cfg.enableNvidia;
+        defaultNetwork.settings.dns_enabled = true;
+        dockerCompat = true;
+        autoPrune.enable = true;
+      };
+    };
+
+    assertions = [
+      {
+        assertion = !(cfg.withPodman && cfg.withDocker);
+        message = "Only one of modules.virt.withPodman or modules.virt.withDocker can be enabled at the same time.";
+      }
+    ];
+  };
+}
