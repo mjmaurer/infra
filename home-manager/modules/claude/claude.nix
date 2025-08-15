694f9e891ec6d464f44ed992115cf8fd621ad4f0
Michael Maurer <mjmaurer777@gmail.com>
2025-08-15 13:38:27 -0600

    refactor(claude): Rename global settings to user settings

--- home-manager/modules/claude/claude.nix
+++ home-manager/modules/claude/claude.nix
@@ -37,10 +37,10 @@ in
       # Claude Code will make updates to this, so we need to make it writable
       # https://github.com/anthropics/claude-code/issues/4808
       ".claude/settings.json.source" = {
-        text = lib.generators.toJSON { } (import ./settings/global-settings.nix);
+        text = lib.generators.toJSON { } (import ./settings/user-settings.nix);
       };
       ".claude/settings.json" = {
-        text = lib.generators.toJSON { } (import ./settings/global-settings.nix);
+        text = lib.generators.toJSON { } (import ./settings/user-settings.nix);
         onChange = ''
           source="${config.home.homeDirectory}/.claude/settings.json.source"
           target="${config.home.homeDirectory}/.claude/settings.json"
