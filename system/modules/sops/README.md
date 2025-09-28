cf78c8a751760b7e1f6b58c215e0fc0a960ff6ab
Michael Maurer <mjmaurer777@gmail.com>
2025-09-28 15:31:05 -0400

    refactor(sops): Restructure sops secrets into separate files and modules

--- system/modules/sops/README.md
+++ system/modules/sops/README.md
@@ -1,3 +1,15 @@
+# Layout
+
+- `module-secrets` - Secrets that may be used in multiple `module-secrets` files
+- `common-secrets` - (mostly) Secrets needed by home-manager programs. If creating a system module, prefer storing sops config in the module (like duplicacy)
+- `vault` - Sops encrypted files
+
+# Adding Secrets
+
+Secrets are enabled based on tags defined in `flake.nix`. Check in the module you want to add to see which tags are enabled (Darwin is always enabled by default)
+
+# Home Manager Integration
+
 On Darwin, using the sops-nix home-manager module would require the host key pair to be root-readable. You could verify this is an issue by trying it and running `cat ~/Library/Logs/SopsNix/stderr`.
 
 Instead, we make mark user readable secrets here, and symlink them as needed (`/run/` and `/User` can't be directly read on nix rebuild without `--impure`)
--- system/modules/sops/default.nix
+++ system/modules/sops/default.nix
@@ -3,200 +3,36 @@
   pkgs,
   username,
   lib,
+  mylib,
   ...
 }:
 let
   cfg = config.modules.sops;
-  minimalSopsFile = ./secrets/minimal.yaml;
-  fullSopsFile = ./secrets/full.yaml;
 in
 {
 
-  options.modules.sops = {
-    enableFullSecrets = lib.mkOption {
-      type = lib.types.bool;
-      default = pkgs.stdenv.isDarwin;
-      description = "Include personal computer secrets.";
-    };
-    enableMinimalSecrets = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Include minimal secrets.";
-    };
-  };
+  imports = [
+    ./common-secrets/ai-secrets.nix
+    ./common-secrets/gpg-secrets.nix
+    ./common-secrets/nixos-host-secrets.nix
+    ./common-secrets/smb-secrets.nix
+
+    ./module-secrets/llm-cli-sops.nix
+    ./module-secrets/shell-sops.nix
+    ./module-secrets/yt-upload-sops.nix
+  ];
 
   config = {
-    sops = lib.mkMerge [
+    sops = {
+      age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      # Generate age key based on above SSH key to this path
+      age.keyFile = "/var/lib/sops-nix/key.txt";
+      age.generateKey = true;
 
-      {
-        age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-        # Generate age key based on above SSH key to this path
-        age.keyFile = "/var/lib/sops-nix/key.txt";
-        age.generateKey = true;
+      # Not using host GPG keys, so unset default
+      gnupg.sshKeyPaths = [ ];
 
-        # Not using host GPG keys, so unset default
-        gnupg.sshKeyPaths = [ ];
-
-        defaultSopsFile = ./secrets/common.yaml;
-        secrets = {
-          smbHost = { };
-          smbPassword = { };
-          gpgAuthKeygrip = { };
-          gpgPublicKey = {
-            owner = config.users.users.${username}.name;
-            mode = "0444";
-          };
-          mjmaurerHashedPassword = {
-            neededForUsers = true;
-            # Allow for this to be overridden by the user
-            sopsFile = lib.mkDefault ./secrets/common.yaml;
-          };
-        };
-      }
-
-      (lib.optionalAttrs cfg.enableMinimalSecrets {
-        secrets = {
-          apiKeyGemini = {
-            sopsFile = minimalSopsFile;
-          };
-        };
-        templates = {
-          "llm-keys-minimal" = {
-            owner = config.users.users.${username}.name;
-            content = builtins.toJSON {
-              gemini = config.sops.placeholder.apiKeyGemini;
-            };
-          };
-        };
-      })
-
-      (lib.optionalAttrs cfg.enableFullSecrets {
-        secrets = {
-          # TODO: probably want this via sops home-manager module,
-          # but it does create issues with (permissions? I tried but can't remember the error)
-          apiKeyAnthropic = {
-            sopsFile = fullSopsFile;
-          };
-          apiKeyCodestral = {
-            sopsFile = fullSopsFile;
-          };
-          apiKeyGemini = {
-            sopsFile = fullSopsFile;
-          };
-          apiKeyVoyage = {
-            sopsFile = fullSopsFile;
-          };
-          apiKeyOpenai = {
-            sopsFile = fullSopsFile;
-          };
-          apiKeyDeepseek = {
-            sopsFile = fullSopsFile;
-          };
-          apiKeyOpenrouter = {
-            sopsFile = fullSopsFile;
-          };
-        };
-        templates = {
-          "pro-25-preview.yaml" = {
-            owner = config.users.users.${username}.name;
-            content = ''
-              name: Gemini Pro 2.5
-              id: gemini-2.5-pro-preview-03-25 
-              apiKey: ${config.sops.placeholder.apiKeyGemini}
-              apiType: google
-              url: https://generativelanguage.googleapis.com
-            '';
-          };
-          "flash-25-preview.yaml" = {
-            owner = config.users.users.${username}.name;
-            content = ''
-              name: Gemini Flash 2.5
-              id: gemini-2.5-flash-preview-04-17
-              apiKey: ${config.sops.placeholder.apiKeyGemini}
-              apiType: google
-              url: https://generativelanguage.googleapis.com
-            '';
-          };
-          "flash-thinking-25-preview-openrouter.yaml" = {
-            owner = config.users.users.${username}.name;
-            content = ''
-              name: Gemini Flash Thinking 2.5 (OpenRouter)
-              id: google/gemini-2.5-flash-preview:thinking
-              apiKey: ${config.sops.placeholder.apiKeyOpenrouter}
-              url: https://openrouter.ai/api/v1
-            '';
-          };
-          "flash-25-preview-openrouter.yaml" = {
-            owner = config.users.users.${username}.name;
-            content = ''
-              name: Gemini Flash 2.5 (OpenRouter)
-              id: google/gemini-2.5-flash-preview
-              apiKey: ${config.sops.placeholder.apiKeyOpenrouter}
-              url: https://openrouter.ai/api/v1
-
-
-            '';
-          };
-          "pro-25-preview-openrouter.yaml" = {
-            owner = config.users.users.${username}.name;
-            content = ''
-              name: Gemini Pro 2.5 (OpenRouter)
-              id: google/gemini-2.5-pro-preview-03-25
-              apiKey: ${config.sops.placeholder.apiKeyOpenrouter}
-              url: https://openrouter.ai/api/v1
-            '';
-          };
-          "o3.yaml" = {
-            owner = config.users.users.${username}.name;
-            content = ''
-              name: o3 
-              id: openai/o3 
-              apiKey: ${config.sops.placeholder.apiKeyOpenrouter}
-              url: https://openrouter.ai/api/v1
-            '';
-          };
-          "o4-mini.yaml" = {
-            owner = config.users.users.${username}.name;
-            content = ''
-              name: o4-mini
-              id: openai/o4-mini
-              apiKey: ${config.sops.placeholder.apiKeyOpenrouter}
-              url: https://openrouter.ai/api/v1
-            '';
-          };
-          "llm-keys-full" = {
-            owner = config.users.users.${username}.name;
-            content = builtins.toJSON {
-              gemini = config.sops.placeholder.apiKeyGemini;
-              openai = config.sops.placeholder.apiKeyOpenai;
-              anthropic = config.sops.placeholder.apiKeyAnthropic;
-            };
-          };
-          "shell.env" = {
-            # mode = "0400";
-            # group = config.users.groups.${username}.name;
-            # restartUnits = [ "home-assistant.service" ];
-            owner = config.users.users.${username}.name;
-            content = ''
-              export ANTHROPIC_API_KEY=${config.sops.placeholder.apiKeyAnthropic}
-              export GEMINI_API_KEY=${config.sops.placeholder.apiKeyGemini}
-              export CLAUDE_API_KEY=${config.sops.placeholder.apiKeyAnthropic}
-              export CODESTRAL_API_KEY=${config.sops.placeholder.apiKeyCodestral}
-              export VOYAGE_API_KEY=${config.sops.placeholder.apiKeyVoyage}
-              export OPENAI_API_KEY=${config.sops.placeholder.apiKeyOpenai}
-              export DEEPSEEK_API_KEY=${config.sops.placeholder.apiKeyDeepseek}
-            '';
-          };
-          "gpg_sshcontrol" = {
-            owner = config.users.users.${username}.name;
-            content = ''
-              ${config.sops.placeholder.gpgAuthKeygrip}
-
-            '';
-            # Newlines in 'content' are needed!
-          };
-        };
-      })
-    ];
+      defaultSopsFile = ./secrets/common.yaml;
+    };
   };
 }
