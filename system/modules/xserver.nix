42522cb6ac47bcf0ddf9ce0df41d5fa54f244b62
Michael Maurer <mjmaurer777@gmail.com>
2025-10-08 15:58:32 -0400

    refactor(xserver): set autorun based on Nvidia detection and simplify displayManager configuration

--- system/modules/xserver.nix
+++ system/modules/xserver.nix
@@ -1,22 +1,21 @@
 { config, lib, ... }:
+let
+  hasNvidia = lib.hasAttrByPath [ "modules" "nvidia" ] config;
+in
 {
   # Nix unfortunately uses "services.xserver" for some wayland
   # configuration for legacy reasons.
   services.xserver = {
     enable = true;
     # Xserver is just needed for lightdm and XWayland I believe.
-    autorun = true;
-    displayManager =
-      let
-        hasNvidia = lib.hasAttrByPath [ "modules" "nvidia" ] config;
-      in
-      {
-        # This was recently added:
-        defaultSession = lib.mkIf (config.programs.sway.enable) "sway";
-        lightdm = {
-          enable = !hasNvidia;
-          greeters.gtk.enable = !hasNvidia;
-        };
+    autorun = hasNvidia;
+    displayManager = {
+      # This was recently added:
+      defaultSession = lib.mkIf (config.programs.sway.enable) "sway";
+      lightdm = {
+        enable = !hasNvidia;
+        greeters.gtk.enable = !hasNvidia;
       };
+    };
   };
 }
