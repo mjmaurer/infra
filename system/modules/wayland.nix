0df5588310ff9a2f16b9261eee0a42faf8abb48b
Michael Maurer <mjmaurer777@gmail.com>
2025-10-08 13:37:59 -0400

    feat(wayland): add NVIDIA/GBM env vars for stability and performance

--- system/modules/wayland.nix
+++ system/modules/wayland.nix
@@ -9,6 +9,14 @@
   programs.sway = {
     enable = true;
     wrapperFeatures.gtk = true;
+    extraSessionCommands = ''
+      # NVIDIA/GBM + wlroots stability/perf
+      # newly added from ll
+      export WLR_RENDERER=vulkan
+      export WLR_NO_HARDWARE_CURSORS=1
+      export GBM_BACKEND=nvidia-drm
+      export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    '';
   };
   # Xwayland for X11 apps under Sway
   programs.xwayland.enable = config.services.xserver.enable;
