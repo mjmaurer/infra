0b7fc8d8288a97fb5b8ebf6ec578aaae168fceb7
Michael Maurer <mjmaurer777@gmail.com>
2025-11-28 02:14:44 -0500

    feat(openwebui): migrate Open WebUI to OCI container and configure state directories

--- system/machines/maple/openwebui.nix
+++ system/machines/maple/openwebui.nix
@@ -1,23 +1,40 @@
 { pkgs-latest, config, ... }:
+let
+  hostStateDir = "/var/lib/open-webui";
+  containerStateDir = "/state";
+in
 {
-  services.open-webui = {
-    enable = true;
-    host = "0.0.0.0";
-    port = 8181;
-    stateDir = "/var/lib/open-webui";
-    package = pkgs-latest.open-webui.overridePythonAttrs (old: {
-      dependencies = old.dependencies ++ [
-        pkgs-latest.python3Packages.itsdangerous
-      ];
-    });
+  virtualisation.oci-containers.containers."open-webui" = {
+    image = "ghcr.io/open-webui/open-webui:latest";
+
+    pull = "always";
+    autoStart = true;
+    autoRemoveOnStop = true;
+    extraOptions = [ "--replace" ];
+
+    ports = [ "0.0.0.0:8181:8080/tcp" ];
+    volumes = [
+      "${hostStateDir}:${containerStateDir}"
+    ];
     environment = {
-      # See https://github.com/NixOS/nixpkgs/pull/431395#issuecomment-3161532401
-      FRONTEND_BUILD_DIR = "${config.services.open-webui.stateDir}/build";
-      SENTENCE_TRANSFORMERS_HOME = "${config.services.open-webui.stateDir}/transformers_home";
+      FRONTEND_BUILD_DIR = "${containerStateDir}/build";
+      SENTENCE_TRANSFORMERS_HOME = "${containerStateDir}/transformers_home";
+      STATIC_DIR = "${containerStateDir}/static";
+      DATA_DIR = "${containerStateDir}/data";
+
+      WEBUI_URL = "http://localhost:8181";
+
       SCARF_NO_ANALYTICS = "True";
       DO_NOT_TRACK = "True";
       ANONYMIZED_TELEMETRY = "False";
     };
-    openFirewall = false;
   };
+
+  systemd.tmpfiles.rules = [
+    "d ${hostStateDir} 0755 root root -"
+    "d ${hostStateDir}/data 0755 root root -"
+    "d ${hostStateDir}/build 0755 root root -"
+    "d ${hostStateDir}/transformers_home 0755 root root -"
+    "d ${hostStateDir}/static 0755 root root -"
+  ];
 }
