0eebc21bbc03942e902d649d3bb3f23699a99cd9
Michael Maurer <mjmaurer777@gmail.com>
2025-11-28 01:39:34 -0500

    chore(maple): add itsdangerous dependency to open-webui and disable firewall

--- system/machines/maple/openwebui.nix
+++ system/machines/maple/openwebui.nix
@@ -4,12 +4,16 @@
     enable = true;
     port = 8181;
     stateDir = "/var/lib/open-webui";
-    package = pkgs-latest.open-webui;
+    package = pkgs-latest.open-webui.overridePythonAttrs (old: {
+      dependencies = old.dependencies ++ [
+        pkgs-latest.python3Packages.itsdangerous
+      ];
+    });
     environment = {
       SCARF_NO_ANALYTICS = "True";
       DO_NOT_TRACK = "True";
       ANONYMIZED_TELEMETRY = "False";
     };
-    openFirewall = true;
+    openFirewall = false;
   };
 }
